742339a21aa3bf4e0566590667890b912a142ee1
Ank <ank@Anks-MBP.local>
2015-10-15 23:40:56 +0200

    all webIR project related files

---
+++ tweetsPreProcessing/tweetsPreProcessing.py
@@ -0,0 +1,54 @@
+import sys;
+import re;
+import os;
+import nltk;
+from nltk import sent_tokenize, word_tokenize, pos_tag
+
+#define a list of Stop Words to be removed from token list
+STOPWORDS = ["a","an","the","is"]
+
+#define regex patterns to remove from token list
+REMOVEPATTERN = ['[! \. ,]+']
+REMOVETAGS = ['@.*','#.*']
+
+#tweet counter
+COUNT = 0
+
+CMD = 'rm preprocessedTweets.csv'
+
+def stopWordRemoval(tokenList):
+	global COUNT
+	COUNT = COUNT+1
+	print 'tweet #',COUNT
+	for sw in STOPWORDS:
+		print "removing ", sw,"..."
+		while sw in tokenList:
+			tokenList.remove(sw)
+	return tokenList
+
+def cleanTokenList(tokenList):
+	for regex in REMOVEPATTERN:
+		tokenList = [val for val in tokenList if re.match(regex,val)==None]
+	return tokenList	
+
+def tokenize_text(text):
+	tokenList = word_tokenize(text)
+	return tokenList
+
+def writeToFile(processedTweet):
+	f = open('preprocessedTweets.csv','a')
+	f.write(processedTweet+'\n')
+
+if __name__ == "__main__":
+	text = '@mycola Hello dere #obama This is  Eindhoven university...lets say somethin abt d city eindhoven.!!!\n it is in nethelands nd  the city is vry nice!,, the weathr is soo cold nowaday!. and v have classes in d mornin. and so many assignmnts which takes most of our time whch realy sucks #barack @mark.'
+	tweetList = text.lower().split('\n')
+	tweetList = cleanTokenList(tweetList)
+	os.system(CMD)
+	for tweet in tweetList:
+		print tweet
+		tokenList = tokenize_text(tweet);
+	##remove stop words but do not modify original words list..send a copy of words list
+		tokenListNoSW = stopWordRemoval(list(tokenList))
+	##remove unnecessary strings
+		tokenListClean = cleanTokenList(tokenListNoSW)
+		writeToFile(' '.join(tokenListClean))
