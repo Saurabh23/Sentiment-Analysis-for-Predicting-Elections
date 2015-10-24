c98215a398dc6d36d824ea85892945b3b592374e
pduy <pham.duy89@gmail.com>
2015-10-24 21:19:27 +0200

    modify the extracting tweets code

--- tweetsPreProcessing/1-extractTweets.py
+++ tweetsPreProcessing/1-extractTweets.py
@@ -1,15 +1,27 @@
 import sys
 
-def main(filename,filenameTweetText):
+def main(filename,filenameTweetText,colName):
 	f = open(filename, 'r+')
 	fw = open(filenameTweetText,'w+')
 	tweets = f.read().split('\n')
+
+        cols = tweets[0].split(',')
+        print cols
+        index = 0
+        for i in range(0, len(cols) - 1):
+            print cols[i]
+            if cols[i] == colName:
+                index = i
+                break
+
+        print index
 	for tweet in tweets:
-		t = tweet.split(';',4)
-		if len(t)==5:
-			fw.write(t[4]+'\n')
-	
+            t = tweet.split(',',len(cols) - 1)
+            print t
+            print len(t)
+	    if len(t) > index:
+	    	fw.write(t[index]+'\n')
 
 if __name__ == "__main__":
-	main(sys.argv[1],sys.argv[2])
+	main(sys.argv[1],sys.argv[2],sys.argv[3])
 		
