5711ab6d414203d02fcc5312c7685fc9698480e0
Duy Pham <pham.duy89@gmail.com>
2016-01-21 04:33:16 +0100

    update penalty function and user-sorted data

--- Code/classification.R
+++ Code/classification.R
@@ -1,225 +1,153 @@
 rm(list= ls()[!(ls() %in% c('top_frequency_terms_merged', 'model'))])
+rm(list = ls(all = TRUE))
 library(tm)
+library(e1071)
 
-############## DAVID CAMERON ############################
-cameron <- read.csv("~/TUE/Quartile1/IRandDM/SentimentAnalysis/WebIR-Full/Data/final_dave.csv", stringsAsFactors = FALSE) 
-miliband <- read.csv("~/TUE/Quartile1//IRandDM/SentimentAnalysis/WebIR-Full/Data/final_ed.csv", sep = ",")
-cameron <- read.csv("~/TUE/Quartile1/IRandDM/SentimentAnalysis/WebIR-Full/Data/Milliband_Tweet.csv", sep = ",")
-cameron <- read.csv("~/TUE/Quartile1/IRandDM/SentimentAnalysis/WebIR-Full/Data/tweets_CAM.csv", sep = ",")
-cameron <- read.csv("~/TUE/Quartile1/IRandDM/SentimentAnalysis/WebIR-Full/Data/SemEvalProcessed.csv", sep = "\t", quote = '\"')
-trainingSet <- read.csv("~/TUE/Quartile1/IRandDM/SentimentAnalysis/WebIR-Full/Data/trainingandtestdata/training.1600000.processed.noemoticon.csv")
-colnames(trainingSet) <- c("polarity", "id", "date", "query", "user", "text")
-summary(cameron)
+rawTrainingData <- read.csv("~/TUE/Quartile1/IRandDM/SentimentAnalysis/WebIR-Full/Data/SemEvalWithoutNeutral.csv", sep = ",")
+rawTrainingData <- read.csv("~/TUE/Quartile1/IRandDM/SentimentAnalysis/WebIR-Full/Data/Milliband_Tweet.csv-sorted.csv", sep = ",", quote = '\"')
+rawTrainingData <- rawTrainingData[1:5000,]
 
-cameron.text <- as.data.frame(cameron$Text)
-cameron.sentiment <- as.data.frame(cameron$FinalSentiment)
-miliband.text <- as.data.frame(miliband$Text)
-miliband.sentiment <- as.data.frame(miliband$Sentiment)
-cameron <- data.frame(cameron.sentiment, cameron.text)
-colnames(cameron) <- c("Sentiment", "Tweets")
-miliband <- data.frame(miliband.sentiment, miliband.text)
-colnames(miliband) <- c("Sentiment", "Tweets")
+pos = scan(
+  "~/TUE/Quartile1/IRandDM/SentimentAnalysis/WebIR-Full/tweetsPreProcessing/data/positive-words.txt", what = 'character', comment.char = ';'
+)
+neg = scan(
+  "~/TUE/Quartile1/IRandDM/SentimentAnalysis/WebIR-Full/tweetsPreProcessing/data/negative-words.txt", what = 'character', comment.char = ';'
+)
 
-cameron <- rbind(miliband, cameron)
+neg <- cbind(neg)
+pos <- cbind(pos)
 
-cameron <- as.data.frame(cameron[0:10000, ])
-cameron <- as.data.frame(cameron[10001:20000, ])
-cameron <- as.data.frame(cameron[20001:27662, ])
+posNegWords <- cbind(pos)
+posNegWords <- rbind(neg, pos)
+rm(neg, pos)
+posNegWords <- as.list(posNegWords)
 
-cameron <- as.data.frame(cameron[20001:30000, ])
-cameron <- as.data.frame(cameron[30001:40000, ])
-cameron <- as.data.frame(cameron[40001:50000, ])
-cameron <- as.data.frame(cameron[50001:56178, ])
-colnames(cameron) <- c("Tweets")
+#Pre-processing methods
+removeURL <- function(x)
+  gsub('http\\S+\\s*',' ', x)
+removeHashTag <- function(x)
+  gsub('#\\S+\\s*',' ', x)
+removeReference <- function(x)
+  gsub('@\\S+', ' ', x)
+removeShortWords <- function(x) gsub("\\b[a-zA-Z0-9]{1,2}\\b", " ", x)
 
-cameronVector <- VectorSource(cameron$Text)
-top_frequency_terms_1 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_2 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_3 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_4 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_5 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_6 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_7 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_8 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_9 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_10 <- getTermFrequencyList(cameronVector)
-top_frequency_terms_merged <- getTermFrequencyList(vector)
+k <- length(rawTrainingData$Text)
+textList <- list()
+for(i in 1: k){
+  #text <- as.String(rawTrainingData$Text[i])
+  text <- rawTrainingData$Text[i]
+  text <- iconv(text, to = 'utf-8', sub = '')
+  text <- tolower(text)
+  text <- removeHashTag(text)
+  text <- removeReference(text)
+  text <- removePunctuation(text)
+  text <- removeShortWords(text)
+  text <- removeURL(text)
+  text <- removeWords(text, stopwords)
+  
+  #Stemming
+  text <- lapply(strsplit(text, " "), stemDocument, language = "english") [[1]]
+  newText <- ""
+  for (word in text) {
+    newText <- paste(newText, word)
+  }
+  text <- newText
+  text <- stripWhitespace(text)
+  
+  textList[i] <- text
+}
 
-trainingVector <- VectorSource(trainingSet$text)
-topFrequencyTerms <- getTermFrequencyList(trainingVector)
-top_frequency_terms_merged<- c(top_frequency_terms_1,
-                                     top_frequency_terms_2,
-                                     top_frequency_terms_3,
-                                     top_frequency_terms_4,
-                                     top_frequency_terms_5,
-                                     top_frequency_terms_6,
-                                     top_frequency_terms_7,
-                                     top_frequency_terms_8,
-                                     top_frequency_terms_9
-                                     )
+##combine sentiment and tweet columns after preprocessing
+text.df <- data.frame(matrix(unlist(textList), nrow = k, byrow = T))
+processedTrainingData <- data.frame(rawTrainingData$Sentiment, text.df$matrix.unlist.textList...nrow...k..byrow...T.)
+processedTrainingData <- text.df
 
-top_frequency_terms_merged <- unique(top_frequency_terms_merged)
+colnames(processedTrainingData) <- c("Sentiment","Text")
+colnames(processedTrainingData) <- c("Text")
 
-myCorpus <- Corpus(cameronVector)
-myCorpus <- tm_map(myCorpus,
-                   content_transformer(function(x) iconv(x, to='UTF-8', sub='byte')),
-                   mc.cores=1)
-myCorpus <- tm_map(myCorpus, removePunctuation, lazy = T)
-myCorpus <- tm_map(myCorpus, stripWhitespace, lazy = T)
-myCorpus <- tm_map(myCorpus, removeWords, stopwords("en"), lazy = T)
-myCorpus <- tm_map(myCorpus, removeWords, '#\\S+\\s*|@\\S+\\s*|http\\S+\\s*', lazy = T)
-myCorpus <- tm_map(myCorpus, content_transformer(tolower), lazy = T)
+#remove variables
+rm(text.df, text, textList, i)
+processedTrainingData1 <- as.data.frame(processedTrainingData[8001:16111,])
+colnames(processedTrainingData1) <- c("Text")
+  
+#VECTOR OF THE TWEETS
+trainingVector <- VectorSource(processedTrainingData1$Text)
+  
+myCorpus <- Corpus(trainingVector)
 
+bigramTokenizer <-
+ function(x) {
+   unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
+ }
+bigramTokenizer <- function(x) {RWeka::NGramTokenizer(x, RWeka::Weka_control(min = 1, max = 2))}
+dtm <- DocumentTermMatrix(myCorpus, control = list(tokenizer = bigramTokenizer))
 dtm <- DocumentTermMatrix(myCorpus)
-dtm <- DocumentTermMatrix(myCorpus, control = list(tolower = TRUE,
-                                                    removePunctuation = TRUE, 
-                                                    stopwords = TRUE,
-                                                    removeNumbers = TRUE,
-                                                    stemCompletion = TRUE))
-matrix <-  data.frame(inspect(dtm))
-dtm2 <- as.matrix(dtm)
-frequency <- colSums(dtm2)
-frequency <- sort(frequency, decreasing = TRUE)
-head(frequency)
-frequency_matrix <- as.matrix(frequency)
 
-top_frequency_terms <- as.matrix(frequency_matrix[1:100,])
-top_frequency_terms <- frequency[1:100]
-top_frequency_terms <- as.list(rownames(as.matrix(top_frequency_terms)))
 
-top_frequency_terms_2 <- as.matrix(frequency_matrix[1:100,])
-top_frequency_terms_2 <- frequency[1:100]
-top_frequency_terms_2 <- as.list(rownames(as.matrix(top_frequency_terms_2)))
-
-top_frequency_terms_3 <- frequency[1:100]
-top_frequency_terms_3 <- as.list(rownames(as.matrix(top_frequency_terms_3)))
 #FILTER the terms in the DTM
-dtm_frequency_terms <- dtm[, colnames(dtm)%in%top_frequency_terms_merged]
+mostFrequentBigrams <- getBigramFrequencyList(trainingVector, 15)
+mostFrequentTerms <- getTermFrequencyList(trainingVector, 30)
+dtm_frequency_terms <- dtm[, colnames(dtm)%in%c(mostFrequentTerms, mostFrequentBigrams)]
+dtm_frequency_terms <- dtm[, colnames(dtm)%in%c(mostFrequentTerms, posNegWords)]
 dtm2 <- as.matrix(dtm_frequency_terms)
-
-matrix <- data.frame(inspect(dtm_frequency_terms))
-matrix <- data.frame(dtm2)
-#wordcloud (for fun)
-library(wordcloud)
-words <- names(frequency)
-wordcloud(words[1:100], frequency[1:100])
-
-data <- data.frame(dtm2, "Final.Sentiment"=as.factor(cameron$Sentiment))
-data <- data.frame(matrix, "Final.Sentiment"=as.factor(mixed_final$Sentiment))
+data <- data.frame(dtm2, "sentiment"=as.factor(rawTrainingData$Sentiment))
 
 #training and testing
-data_train <- data.frame(rbind(data[1:4000,], data[6001:7317,]))
-data_test <- data.frame(rbind(data[4001:6000,]))
+data_train <- data.frame(rbind(data[1:3000,], data[3700:5000,]))
+data_test <- data.frame(rbind(data[3001:3699,]))
 
-data_train <- data.frame(rbind(data[1:4000,], data[6001:10000,]))
-data_test <- data.frame(rbind(data[4001:6000,]))
+model = naiveBayes(as.factor(data_train$sentiment)~., data = data_train, laplace = 0.03)
+pred <- predict(model, data_test, type = "class", threshold = 0.03, esp = 0)
+pred <- predict(model, dtm2, type = "raw", threshold = 0.03, esp = 0)
 
-table(data_train$Final.Sentiment)
-table(data_test$Final.Sentiment)
+pred_prob <- predict(model, data_test, type = "raw", threshold = 0.03, eps = 0)
+pred.df <- as.data.frame(pred)
 
-model = naiveBayes(as.factor(data_train$Final.Sentiment)~., data = data_train)
-model
-pred <-predict(model, data_test,
-               type = "class", threshold = 0.05)
-pred <-predict(model, matrix,
-               type = "class", threshold = 0.05)
+sent <- list()
+for (i in 1:8000) {
+  if (pred.df$positive[i] == 1) {
+    sent[i] <- "positive"
+  } else if (pred.df$negative[i] > 0.5){
+    sent[i] <- "negative"
+  } else {
+    sent[i] <- "neutral"
+  }
+}
+sent <- unlist(sent)
+
+sent.df <- data.frame(unlist(sent))
+summary(sent.df$unlist.sent.)
+
+library(ROCR)
+prediction <- prediction(pred.df$positive, data_test$Final.Sentiment, label.ordering = NULL)
+perf <- performance(prediction, "tpr", "fpr")
+plot(perf)
 #Classification table with test data
-conf.nb=table(pred, as.factor(data_test$Final.Sentiment))
+conf.nb=table(pred, as.factor(data_test$sentiment))
 accuracy.nb <- sum(diag(conf.nb))/nrow(data_test) * 100
 sprintf("The accuracy of the model using naive bayes is %0.2f", accuracy.nb)
 conf.nb
 
-#SVM
+list <- list()
+probs <- list()
+for (i in 1:ncol(sample)) {
+  if (as.numeric(sample[1,i]) != 0) {
+    list[length(list) + 1] <- colnames(sample) [i]
+    probs[length(probs) + 1] <- sample[1,i]
+  }
+}
 
-help(svm)
-library(e1071)
-model.svm <- svm(data_train$Final.Sentiment ~.,data = data_train, method = "C-classification", kernel = "linear", cost = 10, gamma = 0.1)
-model.svm <- svm(data_train,data_train$Final.Sentiment,method = "C-classification", kernel = "radial basis")
-model.svm <- svm(data_train_matrix,data_train$Final.Sentiment,method = "C-classification")
-summary(model.svm)
-pred=predict(model.svm,data_test)
-data_test <- data.frame(data_test, as.list(pred))
-pred=predict(model.svm, matrix)
-#Classification table with test data
-conf.svm <- table(pred, as.factor(data_test$Final.Sentiment))
-conf.svm <- table(pred)
-accuracy.svm <- sum(diag(conf.svm))/nrow(data_test) * 100 sprintf("The accuracy of the model using SVM is %0.2f", accuracy.svm)
-conf.svm
+testingText <- data.frame(rbind(rawTrainingData[3001:3699,]))
+result <- cbind(testingText, pred)
+time <- timestamp()
+write.csv(result, paste("~/Desktop/result" , as.String(time) ,".csv"), quote = T)
 
-
-#Random Forest
-set.seed=1234
-modelrf <- randomForest(as.factor(data_train$Final.Sentiment)~., data = data_train,ntree=10)
-#plot(modelrf)
-key_words <- importance(modelrf)
-write.table(key_words,"D:/myproject/key_words.csv",sep=",",row.names=TRUE)
-pred.rf <- predict(modelrf, data_test,type="class")
-#Classification table with test data 
-conf.rf=table(pred.rf, as.factor(data_test$Final.Sentiment)) 
-accuracy.rf <- sum(diag(conf.rf))/nrow(data_test) * 100 
-sprintf("The accuracy of the model using random forest is %0.2f", accuracy.rf)
-conf.rf
-
-
-############## ED MILLIBAND ############################
-milliband <- read.csv("~/TUE/IRandDM/SentimentAnalysis/WebIR-Full/Data/final_dave.csv", sep = ",")
-millibandVector <- VectorSource(milliband$Text)
-myCorpus <- Corpus(millibandVector)
-dtm <- DocumentTermMatrix(myCorpus, control = list(tolower = TRUE,
-                                                    removePunctuation = TRUE, 
-                                                    stopwords = TRUE,
-                                                    removeNumbers = TRUE,
-                                                    stemCompletion = TRUE))
-                                                    
-
-matrix <-  data.frame(inspect(dtm))
-data <- data.frame(matrix, "Final.Sentiment"=as.factor(milliband$Sentiment))
-data <- data.frame(matrix, "Final.Sentiment"=as.factor(mixed_final$Sentiment), row.names = NULL)
-
-#training and testing
-data_train <- data.frame(rbind(data[1:2000,], data[3001:5000,]))
-data_test <- data.frame(rbind(data[2001:3000,]))
-
-data_train <- data.frame(rbind(data_train[1:2000,], data_train[6001:8000,]))
-data_test <- data.frame(rbind(data[2001:3000,]))
-
-table(data_train$Final.Sentiment)
-table(data_test$Final.Sentiment)
-
-model = naiveBayes(as.factor(data_train$Final.Sentiment)~., data = data_train)
-model
-pred <-predict(model, data_test,
-               type = "class", threshold = 0.05)
-#Classification table with test data
-conf.nb=table(pred, as.factor(data_test$Final.Sentiment))
-accuracy.nb <- sum(diag(conf.nb))/nrow(data_test) * 100
-sprintf("The accuracy of the model using naive bayes is %0.2f", accuracy.nb)
-conf.nb
-
-#SVM
-
-help(svm)
-library(e1071)
-model.svm <- svm(data_train$Final.Sentiment ~., data = data_train, method = "C-classification", kernel = "linear", cost = 10, gamma = 0.1)
-model.svm <- svm(data_train,data_train$Final.Sentiment,method = "C-classification", kernel = "radial basis")
-model.svm <- svm(data_train_matrix,data_train$Final.Sentiment,method = "C-classification")
-summary(model.svm)
-pred=predict(model.svm,data_test)
-#Classification table with test data
-conf.svm <- table(pred, as.factor(data_test$Final.Sentiment))
-accuracy.svm <- sum(diag(conf.svm))/nrow(data_test) * 100
-sprintf("The accuracy of the model using SVM is %0.2f", accuracy.svm)
-conf.svm
-
-#Random Forest
-set.seed=1234
-modelrf <- randomForest(as.factor(data_train$Final.Sentiment)~., data = data_train,ntree=10)
-#plot(modelrf)
-key_words <- importance(modelrf)
-write.table(key_words,"D:/myproject/key_words.csv",sep=",",row.names=TRUE)
-pred.rf <- predict(modelrf, data_test,type="class")
-#Classification table with test data 
-conf.rf=table(pred.rf, as.factor(data_test$Final.Sentiment)) 
-accuracy.rf <- sum(diag(conf.rf))/nrow(data_test) * 100 
-sprintf("The accuracy of the model using random forest is %0.2f", accuracy.rf)
-conf.rf
+library(caret)
+# define training control
+train_control <- trainControl(method="cv", number=10)
+# train the model 
+model <- train(Final.Sentiment~., data=data, trControl=train_control, method="nb")
+# make predictions
+predictions <- predict(model, data)
+# summarize results
+confusionMatrix(predictions, data$Final.Sentiment)
--- Code/crossValidationScript.R
+++ Code/crossValidationScript.R
@@ -51,16 +51,46 @@ filterBigrams <- function(bigrams, wordList) {
 }
 
 trainAndTest <- function(testingText, dataTrain, dataTest, foldSize, method) {
+  print(summary(dataTrain$Final.Sentiment))
   if(method == "NB") {
     model = naiveBayes(as.factor(dataTrain$Final.Sentiment)~., data = dataTrain, laplace = 0.03)
-    pred <- predict(model, dataTest, type = "class", threshold = 0.03, esp = 0)
-    print(summary(pred))
+    pred <- predict(model, dataTest, type = "raw", threshold = 0.03, esp = 0)
     
   } else if(method == "SVM") {
     model <- svm(dataTrain$Final.Sentiment ~., data = dataTrain, method = "C-classification", kernel = "linear", cost = 10, gamma = 0.1)
     pred = predict(model, dataTest)
   }
-  confusionMatrix <- table(pred, as.factor(dataTest$Final.Sentiment))
+  
+  len <- length(dataTest$Final.Sentiment)
+  features <- list()
+  for (i in 1:len) {
+    list <- list()
+    sample <- dataTest[i,]
+    for (i in 1:ncol(sample)) {
+      if (nrow(sample) < 1) {
+        break
+      }
+      if (as.numeric(sample[1,i]) != 0) {
+        list[length(list) + 1] <- colnames(sample) [i]
+      }
+    }
+    features[length(features) + 1] <- paste(list, sep = "", collapse = " ")
+  }
+  
+  sent <- list()
+  pred.df <- as.data.frame(pred)
+  for (i in 1:len) {
+    if (pred.df$positive[i] == 1) {
+      sent[i] <- "positive"
+    } else {
+      sent[i] <- "negative"
+    }
+  }
+  sent <- unlist(sent)
+  
+  print(length(pred))
+  print(length(dataTest$Final.Sentiment))
+  confusionMatrix <- table(sent, as.factor(dataTest$Final.Sentiment))
   
   accuracy <- sum(diag(confusionMatrix))/nrow(dataTest) * 100
   truepositives <- confusionMatrix[4]
@@ -87,9 +117,16 @@ trainAndTest <- function(testingText, dataTrain, dataTest, foldSize, method) {
   time <- timestamp()
   write.table(confusionMatrix, paste("~/Desktop/confusion-", method ,"-fold", as.String(time), ".txt"))
   
-  result <- cbind(testingText, pred)
+  features <- unlist(features)
+  result <- cbind(testingText, sent, features)
   time <- timestamp()
   write.csv(result, paste("~/Desktop/result" , as.String(time) ,".csv"), quote = T)
+  
+#   UNCOMMENT IF RUNNING WITHOUT CROSS VALIDATION AND NEED AN ROC CURVE
+#   require(ROCR)
+#   prediction <- prediction(pred.df$positive, dataTest$Final.Sentiment, label.ordering = NULL)
+#   perf <- performance(prediction, "tpr", "fpr")
+#   plot(perf)
   return (statistics)
 }
 
@@ -107,6 +144,7 @@ performClassification <- function(rawTrainingData, method, isCrossValidation, lm
   posNegWords <- rbind(neg, pos)
   rm(neg, pos)
   posNegWords <- as.list(posNegWords)
+  stopwords <- scan("~/Desktop/stopwords.txt", what = 'character')
   
   #Pre-processing methods
   removeURL <- function(x)
@@ -132,7 +170,7 @@ performClassification <- function(rawTrainingData, method, isCrossValidation, lm
     text <- stripWhitespace(text)
     
     if (isStopWord) {
-      text <- removeWords(text, stopwords("en"))
+      text <- removeWords(text, stopwords)
     }
     
     #Stemming
@@ -167,9 +205,9 @@ performClassification <- function(rawTrainingData, method, isCrossValidation, lm
       function(x) {
         unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
       }
-    dtm <- DocumentTermMatrix(myCorpus, control = list(weighting=weightTfIdf, tokenize=bigramTokenizer))
+    dtm <- DocumentTermMatrix(myCorpus, control = list(tokenize=bigramTokenizer))
     if (feature == "MFT") {
-      bigramFrequenTerms <- getBigramFrequencyList(trainingVector, 10)
+      bigramFrequentTerms <- getBigramFrequencyList(trainingVector, 10)
       dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%bigramFrequentTerms]
     } else if (feature == "huliu") {
       bigramFrequentTerms <- getBigramFrequencyList(trainingVector, 10)
@@ -179,45 +217,48 @@ performClassification <- function(rawTrainingData, method, isCrossValidation, lm
       mostFrequentTerms <- getTermFrequencyList(trainingVector, 15)
       bigramFrequentTerms1 <- filterBigrams(dtm, posNegWords)
       bigramFrequentTerms2 <- filterBigrams(dtm, mostFrequentTerms)
-      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%bigramFrequentTerms1
-                                                           && colnames(dtm)%in%bigramFrequentTerms2]
+      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%c(bigramFrequentTerms1,
+                                                           bigramFrequentTerms2)]
     }
   } else if (lm == "unigram") {
-    dtm <- DocumentTermMatrix(myCorpus, control = list(weighting= weightTfIdf))
+    dtm <- DocumentTermMatrix(myCorpus) 
     if (feature == "MFT") {
       mostFrequentTerms <- getTermFrequencyList(trainingVector, 15)
       dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%mostFrequentTerms]
     } else if (feature == "huliu") {
       dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%posNegWords]
     } else if (feature == "mix") {
-      mostFrequentTerms <- getTermFrequencyList(trainingVector, 15)
-      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%mostFrequentTerms
-                                                           || colnames(dtm)%in%posNegWords]
+      mostFrequentTerms <- getTermFrequencyList(trainingVector, 30)
+      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in% c(mostFrequentTerms
+                                                           ,posNegWords)]
     }
   } else if (lm == "mix") {
     bigramTokenizer <- function(x) {RWeka::NGramTokenizer(x, RWeka::Weka_control(min = 1, max = 2))}
-    dtm <- DocumentTermMatrix(myCorpus, control = list(weighting=weightTfIdf, tokenize=bigramTokenizer))
+    dtm <- DocumentTermMatrix(myCorpus, control = list(tokenize=bigramTokenizer))
     if (feature == "MFT") {
       mostFrequentTerms <- getTermFrequencyList(trainingVector, 20)
-      bigramFrequentTerms <- getBigramFrequencyList(trainingVector, 10)
-      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%bigramFrequentTerms
-                                      ||colnames(dtm)%in%mostFrequentTerms]
+      bigramFrequentTerms <- getBigramFrequencyList(trainingVector, 15)
+      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%c(bigramFrequentTerms,mostFrequentTerms)]
     } else if (feature == "huliu") {
       bigramFrequentTerms <- getBigramFrequencyList(trainingVector, 10)
       bigramFrequentTerms <- filterBigrams(bigramFrequentTerms, posNegWords)
-      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%bigramFrequentTerms
-                                      || colnames(dtm)%in%posNegWords]
+      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in% c(bigramFrequentTerms,
+                                                                posNegWords)]
     } else if (feature == "mix") {
       mostFrequentTerms <- getTermFrequencyList(trainingVector, 15)
-      bigramFrequentTerms <- getBigramFrequencyList(trainingVector, 10)
-      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in%mostFrequentTerms
-                                                           || colnames(dtm)%in%bigramFrequentTerms
-                                      ||colnames(dtm)%in%posNegWords]
+      bigramFrequentTerms <- getBigramFrequencyList(trainingVector, 15)
+      dtmFromMostFrequentTerms <- dtm[, colnames(dtm)%in% c(mostFrequentTerms,
+                                                           bigramFrequentTerms,
+                                                           posNegWords)]
     }
   }
   
+  rm(dtm)
   matrixFromMostFrequentTerms <- as.matrix(dtmFromMostFrequentTerms)
+  #rm(list= ls()[!(ls() %in% c('matrixFromMostFrequentTerms', 'rawTrainingData',
+  #                            'method', 'isCrossValidation', 'lm', 'feature', 'isStem', 'isStopWord'))])
   data <- data.frame(matrixFromMostFrequentTerms, "Final.Sentiment"=as.factor(rawTrainingData$Sentiment))
+  rm(matrixFromMostFrequentTerms)
   
   len <- length(rawTrainingData$Text)
   foldSize <- len / 10
@@ -236,9 +277,9 @@ performClassification <- function(rawTrainingData, method, isCrossValidation, lm
       testBegin <- (i - 1) * foldSize + 1
       print(testBegin)
       print(testBegin + foldSize)
-      dataTest <- data.frame(rbind(data[testBegin:(testBegin + foldSize),]))
+      dataTest <- data.frame(rbind(data[testBegin:(testBegin + foldSize - 1),]))
       dataTrain <- data.frame(rbind(data[1:testBegin,], data[(testBegin + foldSize + 1):len,]))
-      testingText <- data.frame(rbind(rawTrainingData[testBegin:(testBegin + foldSize),]))
+      testingText <- data.frame(rbind(rawTrainingData[testBegin:(testBegin + foldSize - 1),]))
       
       print(paste("test size: ", length(dataTest$Final.Sentiment)))
       print(paste("train size: ", length(dataTrain$Final.Sentiment)))
@@ -264,25 +305,25 @@ performClassification <- function(rawTrainingData, method, isCrossValidation, lm
     model <- trainAndTest(testingText, dataTrain, dataTest, foldSize, method)
   }
   
-  print(paste("mean accuracy = ", mean(as.numeric(accuracies))))
-  print(paste("sd accuracy = ", sd(as.numeric(accuracies))))
-  
-  print(paste("mean pos precision= ", mean(as.numeric(posPrecisions))))
-  print(paste("sd pos precision= ", sd(as.numeric(posPrecisions))))
-  
-  print(paste("mean pos recall = ", mean(as.numeric(posRecalls))))
-  print(paste("sd pos recall = ", sd(as.numeric(posRecalls))))
-  
-  print(paste("mean neg precision = ", mean(as.numeric(negPrecisions))))
-  print(paste("sd neg precision = ", sd(as.numeric(negPrecisions))))
-  
-  print(paste("mean neg recall = ", mean(as.numeric(negRecalls))))
-  print(paste("sd neg recall = ", sd(as.numeric(negRecalls))))
-  
-  print(paste("mean pos f1 = ", mean(as.numeric(posf1))))
-  print(paste("sd pos f1 = ", sd(as.numeric(posf1))))
-  
-  print(paste("mean neg f1 = ", mean(as.numeric(negf1))))
-  print(paste("sd pos f1 = ", sd(as.numeric(negf1))))
+#   print(paste("mean accuracy = ", mean(as.numeric(accuracies))))
+#   print(paste("sd accuracy = ", sd(as.numeric(accuracies))))
+#   
+#   print(paste("mean pos precision= ", mean(as.numeric(posPrecisions))))
+#   print(paste("sd pos precision= ", sd(as.numeric(posPrecisions))))
+#   
+#   print(paste("mean pos recall = ", mean(as.numeric(posRecalls))))
+#   print(paste("sd pos recall = ", sd(as.numeric(posRecalls))))
+#   
+#   print(paste("mean neg precision = ", mean(as.numeric(negPrecisions))))
+#   print(paste("sd neg precision = ", sd(as.numeric(negPrecisions))))
+#   
+#   print(paste("mean neg recall = ", mean(as.numeric(negRecalls))))
+#   print(paste("sd neg recall = ", sd(as.numeric(negRecalls))))
+#   
+#   print(paste("mean pos f1 = ", mean(as.numeric(posf1))))
+#   print(paste("sd pos f1 = ", sd(as.numeric(posf1))))
+#   
+#   print(paste("mean neg f1 = ", mean(as.numeric(negf1))))
+#   print(paste("sd pos f1 = ", sd(as.numeric(negf1))))
   return (model)
 }
